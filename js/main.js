2666d2c38ccc3658f059c25e8e76e74bdd535765
jomarquin <35999373+jomarquin@users.noreply.github.com>
2020-08-01 15:16:24 -0500

    quitando sidenav

--- js/main.js
+++ js/main.js
@@ -1,83 +1,52 @@
-$(document).ready(function(){
-    $('.NavLateral-DropDown').on('click', function(e){
-        e.preventDefault();
-        var DropMenu=$(this).next('ul');
-        var CaretDown=$(this).children('i.NavLateral-CaretDown');
-        DropMenu.slideToggle('fast');
-        if(CaretDown.hasClass('NavLateral-CaretDownRotate')){
-            CaretDown.removeClass('NavLateral-CaretDownRotate');    
-        }else{
-            CaretDown.addClass('NavLateral-CaretDownRotate');    
-        }
-         
+document.addEventListener('DOMContentLoaded', function() {
+
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+
+    //Inicializamos todos los slider con esta configuración
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, {
+        interval: 4000,
+        height: 300
     });
-    $('.tooltipped').tooltip({delay: 50});
-    $('.ShowHideMenu').on('click', function(){
-        var MobileMenu=$('.NavLateral');
-        if(MobileMenu.css('opacity')==="0"){
-            MobileMenu.addClass('Show-menu');   
-        }else{
-            MobileMenu.removeClass('Show-menu'); 
-        }   
-    }); 
-    $('.btn-ExitSystem').on('click', function(e){
-        e.preventDefault();
-        swal({ 
-            title: "You want out of the system?",   
-            text: "The current session will be closed and will leave the system",   
-            type: "warning",   
-            showCancelButton: true,   
-            confirmButtonColor: "#DD6B55",   
-            confirmButtonText: "Yes",
-            animation: "slide-from-top",   
-            closeOnConfirm: false,
-            cancelButtonText: "Cancel"
-        }, function(){   
-            window.location='index.html'; 
-        });
-    }); 
-    $('.btn-Search').on('click', function(e){
-        e.preventDefault();
-        swal({   
-            title: "What are you looking for?",   
-            text: "Write what you want",   
-            type: "input",   
-            showCancelButton: true,   
-            closeOnConfirm: false,   
-            animation: "slide-from-top",   
-            inputPlaceholder: "Write here",
-            confirmButtonText: "Search",
-            cancelButtonText: "Cancel" 
-        }, function(inputValue){   
-            if (inputValue === false) return false;      
-            if (inputValue === "") {     swal.showInputError("You must write something");     
-            return false   
-            }      
-            swal("Nice!", "You wrote: " + inputValue, "success"); 
-        });    
+
+    //Inicializamos el slider para movil con esta configuración
+    var elems = document.querySelector('#sliderMovil');
+    var instances = M.Slider.init(elems, {
+        interval: 3000,
+        height: 230
     });
-    $('.btn-Notification').on('click', function(){
-        var NotificationArea=$('.NotificationArea');
-        if(NotificationArea.hasClass('NotificationArea-show')){
-            NotificationArea.removeClass('NotificationArea-show');
-        }else{
-            NotificationArea.addClass('NotificationArea-show');
-        }
-    });     
+
+    //Inicializamos el slider para ads con esta configuración
+    var elems = document.querySelector('#sliderAds');
+    var instances = M.Slider.init(elems, {
+        interval: 4000,
+        height: 600,
+        indicators: false
+    });
+    
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+                
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems);
+
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      hoverEnabled: false
+    });
+
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems);
+
 });
-(function($){
-    $(window).load(function(){
-        $(".NavLateral-content").mCustomScrollbar({
-            theme:"light-thin",
-            scrollbarPosition: "inside",
-            autoHideScrollbar: true,
-            scrollButtons:{ enable: true }
-        });
-        $(".ContentPage, .NotificationArea").mCustomScrollbar({
-            theme:"dark-thin",
-            scrollbarPosition: "inside",
-            autoHideScrollbar: true,
-            scrollButtons:{ enable: true }
-        });
-    });
-})(jQuery);
+    
